fb1d48b88c7f30b6b50a4af3d7c222fc8597f92a
HTT <103245076+htt0713@users.noreply.github.com>
2022-05-18 16:20:04 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace area{
+    //% block="circle radius $radius"
+    //% radius.defl=100
+    //% radius.min=0 radius.max=1000
+    export function circle(radius:number) {
+        return radius*radius*Math.PI;
+    }
+}
