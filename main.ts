357b937bfce3e8927600748e2947b93e37f2273b
HTT <103245076+htt0713@users.noreply.github.com>
2022-05-20 17:10:45 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -191,6 +191,8 @@ namespace flybit{
             buf.setNumber(NumberFormat.Int16LE, 1, ax)
             buf.setNumber(NumberFormat.Int16LE, 3, ay)
             radio.sendBuffer(buf)
+
+            basic.pause(100)
         }
     }
 
