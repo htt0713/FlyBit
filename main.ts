7cc1cba385782a3c41c9300867a0cf84d585d7a2
HTT <103245076+htt0713@users.noreply.github.com>
2022-05-20 17:44:08 +0800

    edit launchBlock

--- main.ts
+++ main.ts
@@ -271,29 +271,45 @@ namespace flybit{
 
     //% block="Land Feature" 
     export function land(){
-        //disarm every time this function is called
-        let b = 0
-        let a = 0
         let ax = 0
         let ay = 0
-        let buttonState = 0
-        let axRx = 0
-        let ayRx = 0
+        let buttonState = 2
         let buf = pins.createBuffer(5)
+
+        ax = input.acceleration(Dimension.X) + 2020
+        ay = input.acceleration(Dimension.Y) + 2060
+        buttonState = 1
         buf.setNumber(NumberFormat.Int16LE, 0, buttonState)
         buf.setNumber(NumberFormat.Int16LE, 1, ax)
         buf.setNumber(NumberFormat.Int16LE, 3, ay)
         radio.sendBuffer(buf)
-        //skip to 9
-        for (let i = 0; i < 10; i++) {
-            a = 1 //decrease in the state machine
-            buttonState = a + 2 * b
-            buf.setNumber(NumberFormat.Int16LE, 0, buttonState)
-            buf.setNumber(NumberFormat.Int16LE, 1, ax)
-            buf.setNumber(NumberFormat.Int16LE, 3, ay)
-            radio.sendBuffer(buf)
-            basic.pause(200)
-        }
+        basic.pause(100)
+
+        buttonState = 0
+        buf.setNumber(NumberFormat.Int16LE, 0, buttonState)
+        buf.setNumber(NumberFormat.Int16LE, 1, ax)
+        buf.setNumber(NumberFormat.Int16LE, 3, ay)
+        radio.sendBuffer(buf)
+
+        basic.pause(100)
+
+        radio.onReceivedNumberDeprecated(function (receivedNumber) {
+            if (receivedNumber == 1) {
+                basic.clearScreen()
+            }
+            rxNumberCurrent = receivedNumber
+            led.plot(receivedNumber % 5, receivedNumber / 5)
+
+            if (rxNumberCurrent < rxNumberPrev) {
+                led.unplot((receivedNumber + 1) % 5, (receivedNumber + 1) / 5)
+            }
+            if (receivedNumber == 0) {
+                basic.clearScreen()
+                led.plotAll()
+            }
+            rxNumberPrev = rxNumberCurrent
+        })
+
     }
 
     //% block="Direction | In tandem with Launch" 
